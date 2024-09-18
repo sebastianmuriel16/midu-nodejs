733a945f5b08377e22868eeeceaeba959c90968e
sebastian <muriel.sebastian@correounivalle.edu.co>
2024-09-17 23:01:49 -0500

    inyeccion de dependencias node.js

--- clase-5/app.js
+++ clase-5/app.js
@@ -1,18 +1,26 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.router.js'
+import { createMovieRouter } from './routes/movies.router.js'
 import { corsMiddleware } from './middlewares/cors.js'
+
 // como leer un json en ESModules
 // import fs from 'fs'
 // const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
 
 // como leer un json en EsModules recomendado por ahora
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware)
-app.disable('x-powered-by')
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
+  app.use(corsMiddleware)
+  app.disable('x-powered-by')
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
+
+  const PORT = process.env.PORT ?? 1234
+  app.listen(PORT, () => {
+    console.log(`escuchando en el puerto http://localhost:${PORT}`)
+  })
+}
 
 // app.get('/', (req, res) => {
 //   res.json({ Message: 'Hello World' })
@@ -33,8 +41,3 @@ app.use('/movies', moviesRouter)
 // app.patch('/movies/:id', todo)
 
 // app.delete('/movies/:id', todo)
-
-const PORT = process.env.PORT ?? 1234
-app.listen(PORT, () => {
-  console.log(`escuchando en el puerto http://localhost:${PORT}`)
-})
--- clase-5/controllers/movies.js
+++ clase-5/controllers/movies.js
@@ -1,32 +1,36 @@
 // import { MovieModel } from '../models/movie.js'
-import { MovieModel } from '../models/mysql/movie.js'
+// import { MovieModel } from '../models/mysql/movie.js'
 import { validateMovie, partialValidateMovie } from '../schemas/movies.js'
 export class MovieController {
-  static async getAll (req, res) {
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel
+  }
+
+  getAll = async (req, res) => {
     const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
+    const movies = await this.movieModel.getAll({ genre })
     // que es lo que se renderiza
     res.json(movies)
   }
 
-  static async getById (req, res) {
+  getById = async (req, res) => {
     const { id } = req.params
-    const movie = await MovieModel.getById({ id })
+    const movie = await this.movieModel.getById({ id })
     if (movie) return res.json(movie)
     res.status(404).json({ message: 'Movie not found' })
   }
 
-  static async create (req, res) {
+  create = async (req, res) => {
     const result = validateMovie(req.body)
 
     if (!result.success) {
       return res.status(400).json({ error: JSON.parse(result.error.message) })
     }
-    const newMovie = await MovieModel.create({ input: result.data })
+    const newMovie = await this.movieModel.create({ input: result.data })
     res.status(201).json(newMovie)
   }
 
-  static async delete (req, res) {
+  delete = async (req, res) => {
     //   const origin = req.header('origin')
 
     //   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
@@ -34,7 +38,7 @@ export class MovieController {
     //   }
     const { id } = req.params
 
-    const result = await MovieModel.delete({ id })
+    const result = await this.movieModel.delete({ id })
 
     if (!result) return res.status(404).json({ message: 'Movie not found' })
 
@@ -46,11 +50,11 @@ export class MovieController {
     // res.status(404).json({ message: 'Movie not found' })
   }
 
-  static async update (req, res) {
+  update = async (req, res) => {
     const { id } = req.params
     const result = partialValidateMovie(req.body)
 
-    const updatedMovie = await MovieModel.update({ id, input: result.data })
+    const updatedMovie = await this.movieModel.update({ id, input: result.data })
 
     return res.json(updatedMovie)
   }
